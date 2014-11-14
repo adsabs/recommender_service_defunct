6fbe8a59b2c26dae5f83edbbf8828056b208c4b6
Edwin Henneken <ehenneken@cfa.harvard.edu>
2014-11-14 15:58:05 -0500

    Removed example routes

--- recommender/views.py
+++ recommender/views.py
@@ -12,7 +12,7 @@ blueprint = Blueprint(
 
 class Recommender(Resource):
     """"Return recommender results for a given bibcode"""
-    scopes = 'oauth:recommender:read'
+    scopes = []
     def get(self, bibcode):
        try:
            results = get_recommendations(bibcode)
@@ -22,7 +22,7 @@ class Recommender(Resource):
 
 class Resources(Resource):
   '''Overview of available resources'''
-  scopes = ['oauth:sample_application:read','oauth_sample_application:logged_in']
+  scopes = []
   def get(self):
     func_list = {}
     for rule in current_app.url_map.iter_rules():
@@ -31,27 +31,3 @@ class Resources(Resource):
                               'description': current_app.view_functions[rule.endpoint].view_class.__doc__,
                               }
     return func_list, 200
-
-class UnixTime(Resource):
-  '''Returns the unix timestamp of the server'''
-  scopes = ['oauth:sample_application:read','oauth_sample_application:logged_in']
-  def get(self):
-    return {'now': time.time()}, 200
-
-class PrintArg(Resource):
-  '''Returns the :arg in the route'''
-  scopes = ['oauth:sample_application:read','oauth:sample_application:logged_in'] 
-  def get(self,arg):
-    return {'arg':arg}, 200
-
-class ExampleApiUsage(Resource):
-  '''This resource uses the app.client.session.get() method to access an api that requires an oauth2 token, such as our own adsws'''
-  scopes = ['oauth:sample_application:read','oauth:sample_application:logged_in','oauth:api:search'] 
-  def get(self):
-    r = current_app.client.session.get('http://api.adslabs.org/v1/search')
-    try:
-      r = r.json()
-      return {'response':r, 'api-token-which-should-be-kept-secret':current_app.client.token}, 200
-    except: #For the moment, 401s are not JSON encoded; this will be changed in the future
-      r = r.text
-      return {'raw_response':r, 'api-token-which-should-be-kept-secret':current_app.client.token}, 501
