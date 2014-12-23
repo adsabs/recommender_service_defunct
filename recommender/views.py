7796d6677757d8ca7c014acc0d325a6881a83765
Edwin Henneken <ehenneken@cfa.harvard.edu>
2014-12-23 13:14:26 -0500

    blueprint update

--- recommender/views.py
+++ recommender/views.py
@@ -1,6 +1,8 @@
 from flask import current_app, Blueprint, request
 from flask.ext.restful import Resource
 import time
+import inspect
+import sys
 
 from utils.recommender import get_recommendations
 
@@ -23,11 +25,19 @@ class Recommender(Resource):
 class Resources(Resource):
   '''Overview of available resources'''
   scopes = []
+  rate_limit = [1000,60*60*24]
   def get(self):
     func_list = {}
+
+    clsmembers = [i[1] for i in inspect.getmembers(sys.modules[__name__], inspect.isclass)]
     for rule in current_app.url_map.iter_rules():
-      func_list[rule.rule] = {'methods':current_app.view_functions[rule.endpoint].methods,
-                              'scopes': current_app.view_functions[rule.endpoint].view_class.scopes,
-                              'description': current_app.view_functions[rule.endpoint].view_class.__doc__,
-                              }
+      f = current_app.view_functions[rule.endpoint]
+      #If we load this webservice as a module, we can't guarantee that current_app only has these views
+      if not hasattr(f,'view_class') or f.view_class not in clsmembers:
+        continue
+      methods = f.view_class.methods
+      scopes = f.view_class.scopes
+      rate_limit = f.view_class.rate_limit
+      description = f.view_class.__doc__
+      func_list[rule.rule] = {'methods':methods,'scopes': scopes,'description': description,'rate_limit':rate_limit}
     return func_list, 200
