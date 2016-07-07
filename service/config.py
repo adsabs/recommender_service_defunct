87812d0e558ab4df18d372ff0ed5aab4d06ce424
Edwin Henneken <ehenneken@cfa.harvard.edu>
2016-07-07 11:56:17 -0400

    Github94 + updated unittest

--- service/config.py
+++ service/config.py
@@ -1,14 +1,21 @@
 import os
 
 RECOMMENDER_SECRET_KEY = 'this should be changed'
+# Maximum number of rows for Solr query
 RECOMMENDER_MAX_HITS = 10000
-RECOMMENDER_MAX_INPUT = 500
-RECOMMENDER_CHUNK_SIZE = 100
+# The maximum number of closest papers in a cluster to use in calculations
 RECOMMENDER_MAX_NEIGHBORS = 40
+# How many recommendations are generated
 RECOMMENDER_NUMBER_SUGGESTIONS = 10
-RECOMMENDER_THRESHOLD_FREQUENCY = 1
+# Specify the endpoint for Solr queries
 RECOMMENDER_SOLR_PATH = 'https://api.adsabs.harvard.edu/v1/search/query'
+# Where are the data files used for cluster calculations
 RECOMMENDER_CLUSTER_PROJECTION_PATH = 'data/clusters'
+# Recommendations for publications before this year are very likely to be meaningless
+RECOMMENDER_FROM_YEAR = 1997
+# Recommendations for other journals than these are probably meaningless
+RECOMMENDER_ALLOWED_JOURNALS = ['ApJ..','ApJS.','AJ...','MNRAS','A&A..','ARA&A','PhRvD','A&ARv','PASP.','PASJ.','A&C..']
+# Specify where the recommender database lives
 SQLALCHEMY_BINDS = {
     'recommender': 'postgresql+psycopg2://user:pwd@localhost:5432/recommender'}
 # In what environment are we?
